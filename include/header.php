7d04e5351b74b8c59e523d60d7e10d90f8a0b8f0
TheRermz <mfischer74@gmail.com>
2021-10-20 14:48:02 -0300

    arquivo de include do header

---
+++ include/header.php
@@ -0,0 +1,34 @@
+<header>
+    <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                    <h4 class="text-white">Sobre Mim</h4>
+                    <p class="text-muted">Olá! Me chamo Murilo Fischer e sou apaixonado por tecnologia, jogos e ciência.
+                        Neste site será compartilhado algumas análises de jogos, filmes, séries e notícias que vi!</p>
+                </div>
+                <div class="col-sm-4 offset-md-1 py-4">
+                    <h4 class="text-white">Contact</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="twitter.com/therermz" class="text-white text-header"> <span data-feather="twitter"></span> &nbsp; Me siga no twitter!</a></li>
+                        <li><a href="#" class="text-white text-header"> <span data-feather="twitch"></span> &nbsp; Me
+                                siga na twitch!</a></li>
+                        <li><a href="#" class="text-white text-header"> <span data-feather="mail"></span> &nbsp;Email
+                                me</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+            <a href="#" class="navbar-brand d-flex align-items-center">
+                <img src="assets/img/Therermz joia.png" alt="murilo fazendo joinha" width="50" height="50">
+                <strong>&nbsp; Hellmas once said</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+</header>
