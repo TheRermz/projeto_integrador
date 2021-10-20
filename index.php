998d7485100a8c3f4f1d1d7802b978994edece7d
TheRermz <mfischer74@gmail.com>
2021-10-20 15:42:21 -0300

    Adição de botão de login

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,700;1,300;1,700&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/feather-icons"></script>
-    <title>Hellmas Reviews</title>
+    <meta name="description" content="Análise de jogos, filmes e séries das quais Murilo Fischer aka Hellmas já assistiu">
+    <title>Hellmas once said - Landing Page</title>
 
 </head>
 
@@ -23,18 +24,7 @@
 
     <main>
 
-        <section class="py-5 text-center container">
-            <div class="row py-lg-5">
-                <div class="col-lg-6 col-md-8 mx-auto">
-                    <h1 class="fw-light">Album example</h1>
-                    <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                    <p>
-                        <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                        <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-                    </p>
-                </div>
-            </div>
-        </section>
+
 
         <div class="album py-5 bg-light">
             <div class="container">
@@ -51,8 +41,8 @@
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Veja mais!</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> <!-- acesso apenas por editor/admin -->
                                     </div>
                                     <small class="text-muted">9 mins</small>
                                 </div>
