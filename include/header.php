f4b6a13b674245b024069dab98ec2f3bcd461e51
TheRermz <mfischer74@gmail.com>
2021-10-20 14:57:27 -0300

    Mudanças no header.php e a criação do connect.php

--- include/header.php
+++ include/header.php
@@ -4,17 +4,15 @@
             <div class="row">
                 <div class="col-sm-8 col-md-7 py-4">
                     <h4 class="text-white">Sobre Mim</h4>
-                    <p class="text-muted">Olá! Me chamo Murilo Fischer e sou apaixonado por tecnologia, jogos e ciência.
-                        Neste site será compartilhado algumas análises de jogos, filmes, séries e notícias que vi!</p>
+                    <p class="text-muted">Olá! Meu nome é Murilo, pela internet conhecido como Hellmas, e este site tem como objetivo de fazer análises de jogos que joguei, filmes e séries que assiti. Sou apaixonado por técnologia e busco sempre estar antenado nas novidades. Sinta-se a vontade, pegue um café e vamos conversar</p>
                 </div>
                 <div class="col-sm-4 offset-md-1 py-4">
                     <h4 class="text-white">Contact</h4>
                     <ul class="list-unstyled">
-                        <li><a href="twitter.com/therermz" class="text-white text-header"> <span data-feather="twitter"></span> &nbsp; Me siga no twitter!</a></li>
-                        <li><a href="#" class="text-white text-header"> <span data-feather="twitch"></span> &nbsp; Me
+                        <li><a href="https://twitter.com/therermz" target="_blank" class="text-white text-header"> <span data-feather="twitter"></span> &nbsp; Me siga no twitter!</a></li>
+                        <li><a href="https://twitch.tv/therermz" target="_blank" class="text-white text-header"> <span data-feather="twitch"></span> &nbsp; Me
                                 siga na twitch!</a></li>
-                        <li><a href="#" class="text-white text-header"> <span data-feather="mail"></span> &nbsp;Email
-                                me</a></li>
+                        <li><a href="contato.php" class="text-white text-header"> <span data-feather="mail"></span> &nbsp; Email de contato</a></li>
                     </ul>
                 </div>
             </div>
