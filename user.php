7cf4b86eebdbad5b0d1abab9933a89196de181de
TheRermz <mfischer74@gmail.com>
2021-10-25 16:39:07 -0300

    added user data

--- user.php
+++ user.php
@@ -23,32 +23,101 @@
     include('../projeto_integrador/include/header.php');
     include('../projeto_integrador/include/nav.php');
     ?>
-    <nav class="nav flex-column w-25 float-start py-5">
-        <a class="nav-link active" aria-current="page" href="#">Active</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link" href="#">Link</a>
-        <a class="nav-link disabled">Disabled</a>
+    <nav class="nav flex-column w-25 float-start py-5 align-content-center">
+        <a class="nav-link active" aria-current="page" href="#">Usuário</a>
+        <a class="nav-link" href="#">Alterar dados</a>
+        <a class="nav-link" href="#">Cancelar conta</a>
+        <a class="nav-link" href="#">Adicionar Artigo</a>
+        <a class="nav-link" href="#">Administrativo</a>
     </nav>
+
     <div class="col-12 container py-5">
-        asdasds
-    </div>
-    <footer class="my-5 pt-5 text-muted text-center text-small">
-        <div class="container">
-            <p class="float-end mb-1">
-                <button onclick="gotoTop()" id="t" class="text-primary" title="volta ao topo da página">Voltar ao Topo da página</button>
-            </p>
-            <p class="mb-1"> &copy; <a href="https://www.linkedin.com/in/mfischer-1997/" target="_blank" class="text-pirmary">Murilo Fischer</a> - Todos os direitos Reservados - <span class="text-primary"> 2021</span> </p>
+        <p class="h2">Área do Usuário</p>
+        <p>Olá Usuário!</p>
+        <div class="row g-3">
+            <div class="col-sm-6">
+                <label for="nome" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="nome" placeholder="" value="" readonly>
+
+            </div>
+
+            <div class="col-sm-6">
+                <label for="sobrenome" class="form-label">Sobrenome</label>
+                <input type="text" class="form-control" id="sobrenome" placeholder="" value="" readonly>
+
+            </div>
+
+            <div class="col-12">
+                <label for="username" class="form-label">Username</label>
+                <div class="input-group has-validation">
+                    <span class="input-group-text">@</span>
+                    <input type="text" class="form-control" id="username" placeholder="Username" readonly>
+                </div>
+            </div>
+
+
+            <div class="col-12">
+                <label for="email" class="form-label">Email <span class="text-muted">(Opcional)</span></label>
+                <input type="email" class="form-control" id="email" placeholder="seu@email.com" readonly>
+            </div>
+
+
+            <div class="col-6">
+                <label for="country" class="form-label">País</label>
+                <select class="form-select" id="country" aria-readonly="true">
+                    <option value="select" selected="selected">Selecione um país...</option>
+                </select>
+            </div>
+
+            <div class="col-6">
+                <label for="state" class="form-label">Estado</label>
+                <select class="form-select" id="state" aria-readonly="true">
+                    <option value="">Selecione seu estado...</option>
+
+                </select>
+            </div>
+
+            <p class="h4">Informações adicionais</p>
+            <div class="col-5">
+                <label for="twitchuser" class="form-label"><span data-feather="twitch"></span> Username na twitch</label>
+                <input type="text" class="form-control" id="twitchuser" placeholder="Seu username na twitch" readonly>
+
+            </div>
+            <div class="col-5">
+                <label for="twitteruser" class="form-label"><span data-feather="twitter"></span> Username no twitter Twitter</label>
+                <input type="text" class="form-control" id="twitteruser" placeholder="informe seu twitter" readonly>
+            </div>
+            <div class="col-2">
+                <label for="birthdate" class="form-label"><span data-feather="calendar"></span> Data de Nascimento</label>
+                <input class="form-control" type="date" name="birthdate" id="birthdate" readonly>
+
+            </div>
+
+
+
 
 
 
         </div>
-    </footer>
+
+        <footer class="my-5 pt-5 text-muted text-center text-small">
+            <div class="container">
+                <p class="float-end mb-1">
+                    <button onclick="gotoTop()" id="t" class="text-primary" title="volta ao topo da página">Voltar ao Topo da página</button>
+                </p>
+                <p class="mb-1"> &copy; <a href="https://www.linkedin.com/in/mfischer-1997/" target="_blank" class="text-pirmary">Murilo Fischer</a> - Todos os direitos Reservados - <span class="text-primary"> 2021</span> </p>
 
 
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/misc.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
-    <script>
-        feather.replace()
-    </script>
+
+            </div>
+
+        </footer>
+
+
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/misc.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+        <script>
+            feather.replace()
+        </script>
 </body>
