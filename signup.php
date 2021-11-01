d15366f04593f4ca06846b0295c1db0155b67148
TheRermz <mfischer74@gmail.com>
2021-11-01 17:53:49 -0300

    added php code to insert user

--- signup.php
+++ signup.php
@@ -1,3 +1,50 @@
+<?php
+require_once('connection/connect.php');
+
+#data received via submit btn
+
+if (isset($_POST["insert_user"]) !== '' && $_POST["insert_user"] === 'insert') {
+    $name = mysqli_real_escape_string($con, $_POST["nome"]);
+    $surname = mysqli_real_escape_string($con, $_POST["sobrenome"]);
+    $usrname = mysqli_real_escape_string($con, $_POST["username"]);
+    #   if ($_POST["passwd"] === $_POST["repeatpasswd"]) {
+    $md5passwd = md5(mysqli_real_escape_string($con, $_POST["senha"]));
+    # } #else echo 'A senha ' . $_POST["repeatpasswd"] . ' não corresponde a ' . $_POST["passwd"];
+    $password = mysqli_real_escape_string($con, $_POST["passwd"]);
+    $email = mysqli_real_escape_string($con, $_POST["email"]);
+    $country = mysqli_real_escape_string($con, $_POST["country"]);
+    $state = mysqli_real_escape_string($con, $_POST["state"]);
+    $twitchuser = mysqli_real_escape_string($con, $_POST["twitchuser"]);
+    $twitteruser = mysqli_real_escape_string($con, $_POST["twitteruser"]);
+    $birthdate =  $_POST["birthdate"];
+
+
+    #INSERT QUERY
+
+    $sql = "INSERT INTO users (user_id, name, surname, username, md5_passwd, type, ban, status, reg_day, twitchuser, twitteruser, birthdate, country_id,state_id)
+    VALUES (0, '$name', '$surname', '$usrname', 'md5passwd', 'comum', 0, 1, CURRENT_DATE(), '$twitchuser', '$twitteruser', '$birthdate', $country, $state);";
+    if (mysqli_query($con, $sql)) {
+        header('Location:index.php');
+    } else {
+        die("Erro ao cadastrar o Usuário");
+    }
+}
+
+# pull all countries from the db
+$sqlc = "SELECT * FROM country ORDER BY country_id ASC";
+$queryc = mysqli_query($con, $sqlc);
+$respc = mysqli_fetch_assoc($queryc);
+#echo $respc['country_name_pt'];
+
+# pull all brazilian states from the db
+$sqls = 'SELECT * FROM br_states ORDER BY state_id ASC';
+$qc = mysqli_query($con, $sqls);
+$resps = mysqli_fetch_assoc($qc);
+#echo $resps['state_abbr'];
+
+?>
+
+
 <!doctype html>
 <html lang="pt-br">
 
@@ -34,128 +81,137 @@
 
             <div class="col-md-12">
                 <h4 class="mb-3">Cadastro</h4>
-                <form class="needs-validation" novalidate>
-                    <div class="row g-3">
-                        <div class="col-sm-6">
-                            <label for="nome" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="nome" placeholder="" value="" required>
+                <form class="needs-validation" novalidate ">
+                    <div class=" row g-3">
+                    <div class="col-sm-6">
+                        <label for="nome" class="form-label">Nome</label>
+                        <input type="text" class="form-control" id="nome" placeholder="" value="" required>
+                        <div class="invalid-feedback">
+                            Favor preencher com seu nome.
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <label for="sobrenome" class="form-label">Sobrenome</label>
+                        <input type="text" class="form-control" id="sobrenome" placeholder="" value="" required>
+                        <div class="invalid-feedback">
+                            Favor preencher com seu sobrenome
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <label for="username" class="form-label">Username</label>
+                        <div class="input-group has-validation">
+                            <span class="input-group-text">@</span>
+                            <input type="text" class="form-control" id="username" placeholder="Username" required>
                             <div class="invalid-feedback">
-                                Favor preencher com seu nome.
+                                Favor preencher o username que deseja usar.
                             </div>
                         </div>
-
-                        <div class="col-sm-6">
-                            <label for="sobrenome" class="form-label">Sobrenome</label>
-                            <input type="text" class="form-control" id="sobrenome" placeholder="" value="" required>
+                    </div>
+                    <div class="col-5">
+                        <label for="passwd" class="form-label">Senha</label>
+                        <div class="input-group has-validation">
+                            <input type="password" class="form-control" id="passwd" placeholder="Senha" required>
                             <div class="invalid-feedback">
-                                Favor preencher com seu sobrenome
+                                Favor insira a sua senha.
                             </div>
                         </div>
+                    </div>
+                    <div class="col-5">
+                        <label for="passwdrepeat" class="form-label">Repita a senha</label>
+                        <div class="input-group has-validation">
+                            <input type="password" class="form-control" id="passwdrepeat" placeholder="Indisponível" required readonly>
 
-                        <div class="col-12">
-                            <label for="username" class="form-label">Username</label>
-                            <div class="input-group has-validation">
-                                <span class="input-group-text">@</span>
-                                <input type="text" class="form-control" id="username" placeholder="Username" required>
-                                <div class="invalid-feedback">
-                                    Favor preencher o username que deseja usar.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-5">
-                            <label for="passwd" class="form-label">Senha</label>
-                            <div class="input-group has-validation">
-                                <input type="password" class="form-control" id="passwd" placeholder="Senha" required>
-                                <div class="invalid-feedback">
-                                    Favor insira a sua senha.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-5">
-                            <label for="passwdrepeat" class="form-label">Repita a senha</label>
-                            <div class="input-group has-validation">
-                                <input type="password" class="form-control" id="passwdrepeat" placeholder="Senha" required>
-                                <div class="invalid-feedback">
-                                    Favor repetir a senha.
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-2">
-                            <label>&nbsp;</label>
-                            <div class="checkbox">
-                                <label class="chbox mt-3">
-                                    <input type="checkbox" class="btn btn-group" onclick="showPasswd()"><span class="" data-feather="eye-off" id="passwdIcon" onchange="showPasswd()"></span> Mostrar Senha</button>
-
-                                </label>
-                            </div>
-                        </div>
-
-                        <div class="col-12">
-                            <label for="email" class="form-label">Email <span class="text-muted">(Opcional)</span></label>
-                            <input type="email" class="form-control" id="email" placeholder="seu@email.com">
                             <div class="invalid-feedback">
-                                Informe um email válido.
+                                Favor repetir a senha.
                             </div>
+
                         </div>
+                    </div>
 
+                    <div class="col-2">
+                        <label>&nbsp;</label>
+                        <div class="checkbox">
+                            <label class="chbox mt-3">
+                                <input type="checkbox" class="btn btn-group" onclick="showPasswd()"><span class="" data-feather="eye-off" id="passwdIcon" onchange="showPasswd()"></span> Mostrar Senha</button>
 
-                        <div class="col-md-6">
-                            <label for="country" class="form-label">País</label>
-                            <select class="form-select" id="country" required>
-                                <option value="select" selected="selected">Selecione um país...</option>
-
-                            </select>
-                            <div class="invalid-feedback">
-                                Selecione um país válido
-                            </div>
+                            </label>
                         </div>
+                    </div>
 
-                        <div class="col-md-6">
-                            <label for="state" class="form-label">Estado</label>
-                            <select class="form-select" id="state" required>
-                                <option value="">Selecione seu estado...</option>
-
-                            </select>
-                            <div class="invalid-feedback">
-                                Por favor, selecione seu estado.
-                            </div>
+                    <div class="col-12">
+                        <label for="email" class="form-label">Email <span class="text-muted">(Opcional)</span></label>
+                        <input type="email" class="form-control" id="email" placeholder="seu@email.com">
+                        <div class="invalid-feedback">
+                            Informe um email válido.
                         </div>
-
-                        <p class="h4">Informações adicionais</p>
-                        <div class="col-md-5">
-                            <label for="twitchuser" class="form-label"><span data-feather="twitch"></span> Username na twitch</label>
-                            <input type="text" class="form-control" id="twitchuser" placeholder="Seu username na twitch">
-                            <div class="invalid-feedback">
-                                Informe seu username da twitch.
-                            </div>
-                        </div>
-                        <div class="col-md-5">
-                            <label for="twitteruser" class="form-label"><span data-feather="twitter"></span> Username no twitter Twitter</label>
-                            <input type="text" class="form-control" id="twitteruser" placeholder="informe seu twitter">
-                            <div class="invalid-feedback">
-                                Infome seu user do twitter
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <label for="birthdate" class="form-label"><span data-feather="calendar"></span> Data de Nascimento</label>
-                            <input class="form-control" type="date" name="birthdate" id="birthdate">
-                            <div class="invalid-feedback">
-                                Informe sua data de nascimento.
-                            </div>
-                        </div>
-
                     </div>
 
 
+                    <div class="col-md-6">
+                        <label for="country" class="form-label">País</label>
+                        <select class="form-select" id="country" required>
 
+                            <option value="" selected="selected">Selecione seu país...</option>
+                            <?php do { ?>
+                                <option value="<?php echo $respc['country_id']; ?>"><?php echo $respc['country_name_pt']; ?></option>
+                            <?php } while ($respc = mysqli_fetch_assoc($queryc)); ?>
 
-                    <hr class="my-4">
-                    <div class="d-flex justify-content-center">
-                        <button class="w-50 btn btn-primary btn-lg" type="submit">Finalizar Cadastro</button>
+                        </select>
+                        <div class="invalid-feedback">
+                            Selecione um país válido
+                        </div>
                     </div>
-                </form>
+
+                    <div class="col-md-6">
+                        <label for="state" class="form-label">Estado</label>
+                        <select class="form-select" id="state" required>
+                            <option value="">Selecione seu estado...</option>
+                            <?php do {                            ?>
+                                <option value="<?php echo $resps['state_id'] ?>"><?php echo $resps['state_abbr'] ?></option>
+                            <?php } while ($resps = mysqli_fetch_assoc($qc)); ?>
+                        </select>
+                        <div class="invalid-feedback">
+                            Por favor, selecione seu estado.
+                        </div>
+                    </div>
+
+                    <p class="h4">Informações adicionais</p>
+                    <div class="col-md-5">
+                        <label for="twitchuser" class="form-label"><span data-feather="twitch"></span> Username na twitch</label>
+                        <input type="text" class="form-control" id="twitchuser" placeholder="Seu username na twitch">
+                        <div class="invalid-feedback">
+                            Informe seu username da twitch.
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <label for="twitteruser" class="form-label"><span data-feather="twitter"></span> Username no twitter Twitter</label>
+                        <input type="text" class="form-control" id="twitteruser" placeholder="informe seu twitter">
+                        <div class="invalid-feedback">
+                            Infome seu user do twitter
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <label for="birthdate" class="form-label"><span data-feather="calendar"></span> Data de Nascimento</label>
+                        <input class="form-control" type="date" name="birthdate" id="birthdate">
+                        <div class="invalid-feedback">
+                            Informe sua data de nascimento.
+                        </div>
+                    </div>
+
             </div>
+
+
+
+
+            <hr class="my-4">
+            <div class="d-flex justify-content-center">
+                <input type="hidden" name="insert_user" value="insert">
+                <button class="w-50 btn btn-primary btn-lg" type="submit">Finalizar Cadastro</button>
+            </div>
+            </form>
+    </div>
     </div>
     </main>
 
