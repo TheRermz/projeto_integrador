9a90f67fda8eda8790426578e5cbb81727da68fc
TheRermz <mfischer74@gmail.com>
2021-11-04 11:53:41 -0300

    created sesssion on logon

--- login.php
+++ login.php
@@ -12,20 +12,30 @@ if (isset($_POST["username"]) && $_POST["username"] != '' && isset($_POST["passw
     $passwd = md5(mysqli_real_escape_string($con, $_POST["passwd"]));
 
     $sql = "SELECT * FROM users where username = '$username' AND md5_passwd = '$passwd'";
-    $usrq = mysqli_query($con, $sql);;
+    $usrq = mysqli_query($con, $sql);
     $usrresp = mysqli_fetch_assoc($usrq);
 
     #creating a session for the user
     if (isset($usrresp)) {
         $_SESSION["user_id"] = $usrresp['user_id'];
         $_SESSION["user"] = $usrresp['username'];
-        $_SESSION["type"] = $usrresp['type'];
+        $_SESSION["type"] = $usrresp['user_type'];
         $_SESSION["ban"] = $usrresp['ban'];
+        #  $_SESSION["passwd"] = $usrresp['md5_passwd'];
+        $_SESSION["name"] = $usrresp['user_name'];
+        $_SESSION["surname"] = $usrresp['surname'];
+        $_SESSION["email"] = $usrresp['email'];
+        $_SESSION["country"] = $usrresp['country_id'];
+        $_SESSION["state"] = $usrresp['state_id'];
+        $_SESSION["twitchuser"] = $usrresp['twitchuser'];
+        $_SESSION["twitteruser"] = $usrresp['twitteruser'];
+        $_SESSION["birthdate"] = $usrresp['birthdate'];
 
-        if ($_SESSION["type"] === 'administrador' || $_SESSION["type"] === 'editor' || $_SESSION["type"] === 'comum') {
+        if ($_SESSION["type"] === 'comum' || $_SESSION["type"] === 'editor' || $_SESSION["type"] === 'administrador') {
+
             header('Location:user.php');
         } else {
-            $_SESSION["register"] = 'Apenas usuários cadastrados podem acessar a área de usuário';
+            $_SESSION["register"] = 'Apenas usuários cadastrados podem acessar a área de usuário'; # . $_SESSION["user"] . ' e ' . $_SESSION["passwd"] . ' e ' .  $_SESSION['type'];
             header('Location:signin.php');
         }
         #  if ($usrresp["ban"] === 1) {
@@ -34,9 +44,9 @@ if (isset($_POST["username"]) && $_POST["username"] != '' && isset($_POST["passw
         # }
     } else {
         $_SESSION["loginerror"] = 'Usuário ou Senha inválidos';
-        header('location:signin.php');
+        #  header('location:signin.php');
     }
 } else {
     $_SESSION["emptylogin"] = 'É necessário preencher o usuário e senha';
-    header('location:signin.php');
+    # header('location:signin.php');
 }
