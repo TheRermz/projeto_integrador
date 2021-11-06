8c0ec4ff2ae9268b2ddae961530b592d754745ef
TheRermz <mfischer74@gmail.com>
2021-11-06 18:31:15 -0300

    Now navadmin shows selected page

--- include/navadmin.php
+++ include/navadmin.php
@@ -1,21 +1,20 @@
-<!-- <style>
-    .search { -- todo -- position the search engine below the login btn
-    
-    }
-</style> -->
+<?php
+$activepage = basename($_SERVER["PHP_SELF"], ".php")
+?>
+
 <nav class=" bg-dark navbar-dark">
-    <ul class="nav nav-tabs justify-content-center border-0">
+    <ul class="nav nav-tabs justify-content-center">
         <li class="nav-item">
-            <a class="nav-link active bg-secondary text-white" aria-current="page" href="../">Home</a>
+            <a class="nav-link <?php if ($activepage === 'index') echo 'active bg-secondary', '' ?>  text-white" aria-current="page" href="../projeto_integrador/">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" aria-current="page" href="../about.php">Sobre</a>
+            <a class="nav-link <?php if ($activepage === 'about') echo 'active bg-secondary', '' ?> text-white" aria-current="page" href="../projeto_integrador/about.php">Sobre</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" aria-current="page" href="../recommendations.php">Recomendações</a>
+            <a class="nav-link <?php if ($activepage === 'recommendations') echo 'active bg-secondary', '' ?> text-white" aria-current="page" href="../projeto_integrador/recommendations.php">Recomendações</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" aria-current="page" href="../user.php">Página do Usuário</a>
+            <a class="nav-link <?php if ($activepage === 'user') echo 'active bg-secondary', '' ?> text-white" aria-current="page" href="../projeto_integrador/user.php">Página do Usuário</a>
         </li>
         <!-- <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
