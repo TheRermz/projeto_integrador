ae1aa640b4c367bfa7e82f14ab5e04b1256b1ff8
TheRermz <mfischer74@gmail.com>
2021-11-07 18:39:27 -0300

    pagination added

--- index.php
+++ index.php
@@ -3,11 +3,34 @@ require_once('connection/connect.php');
 if (!isset($_SESSION)) {
     session_start();
 }
-$sqla = "SELECT articles.article_id, articles.article_name, articles.abstract, articles.reg_time, articles.reg_date FROM articles order by article_id DESC LIMIT 10
-";
-$qa = mysqli_query($con, $sqla);
-$resp = mysqli_fetch_assoc($qa);
+// $sqla = "SELECT * FROM articles order by article_id DESC";
+// $qa = mysqli_query($con, $sqla);
+// $resp = mysqli_fetch_assoc($qa);
+// #$qtd = mysqli_num_rows($qa);
+# PAGINATION
 
+$sqlp = "SELECT article_id FROM articles";
+$qp = mysqli_query($con, $sqlp);
+$respq = mysqli_fetch_assoc($qp);
+$qtd = mysqli_num_rows($qp);
+
+if (isset($_GET["page"]) && !empty($_GET["page"])) {
+    $currentpage = $_GET["page"];
+} else {
+    $currentpage = 1;
+}
+$url = "index.php?page=";
+#how many pages will be shown on the pages nav
+$pageqtd = 6;
+#starting value for limit clause
+$initial = ($currentpage * $pageqtd) - $pageqtd;
+$finalpage = ceil($qtd / $pageqtd);
+$firstpage = 1;
+$nextpage = $currentpage + 1;
+$lastpage = $currentpage - 1;
+$sqlpage = "SELECT article_id, article_name, abstract, reg_time, reg_date FROM articles ORDER BY article_id DESC LIMIT $initial, $pageqtd";
+$qpage = mysqli_query($con, $sqlpage);
+$respage = mysqli_fetch_assoc($qpage);
 
 ?>
 
@@ -48,36 +71,57 @@ $resp = mysqli_fetch_assoc($qa);
             <!-- php loop [do while] to load all articles [still an idea > make a featured article] -->
 
             <div class="container">
+
                 <p class="h1 pb-2">Artigos recentes</p>
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <?php if ($qtd > 0) { ?>
+                        <?php do { ?>
+                            <div class="col">
 
-                    <?php do { ?>
-                        <div class="col">
+                                <div class="card shadow-sm">
+                                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        <title>Placeholder</title>
+                                        <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                                    </svg>
 
-                            <div class="card shadow-sm">
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                                    <title>Placeholder</title>
-                                    <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                                </svg>
+                                    <div class="card-body">
+                                        <p class="h5 card-text"><?php echo $respage['article_name'] ?></p>
+                                        <p class="card-text"><?php echo $respage['abstract'] ?></p>
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <div class="btn-group">
+                                                <a class="indexlink" href="article.php?article_id=<?php echo $respage['article_id'] ?> "><button type="button" class="btn btn-sm btn-outline-secondary">Veja Mais</button></a>
 
-                                <div class="card-body">
-                                    <p class="h5 card-text"><?php echo $resp['article_name'] ?></p>
-                                    <p class="card-text"><?php echo $resp['abstract'] ?></p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <a class="indexlink" href="article.php?article_id=<?php echo $resp['article_id'] ?> "><button type="button" class="btn btn-sm btn-outline-secondary">Veja Mais</button></a>
-
-                                            <!-- if user !== comum  -->
-                                            <a class="indexlink mx-1" href="admin/addarticle.php?article_id=<?php echo $resp['article_id'] ?> "><button type="button" class="btn btn-sm btn-outline-secondary">Editar</button></a> <!-- acesso apenas por editor/admin -->
+                                                <!-- if user !== comum  -->
+                                                <a class="indexlink mx-1" href="admin/addarticle.php?article_id=<?php echo $respage['article_id'] ?> "><button type="button" class="btn btn-sm btn-outline-secondary">Editar</button></a> <!-- acesso apenas por editor/admin -->
+                                            </div>
+                                            <small class="text-muted"><?php echo 'Artigo publicado às ' . $respage['reg_time'] ?></small>
                                         </div>
-                                        <small class="text-muted"><?php echo 'Artigo publicado às ' . $resp['reg_time'] ?></small>
                                     </div>
                                 </div>
-                            </div>
 
-                        </div>
-                    <?php } while ($resp = mysqli_fetch_assoc($qa)) ?>
+                            </div>
+                        <?php } while ($respage = mysqli_fetch_assoc($qpage)) ?>
                 </div>
+                <nav class="my-5">
+                    <ul class="pagination justify-content-center">
+                        <?php if ($currentpage >= 2) { ?>
+                            <li class="page-item">
+                                <a class="page-link" href="<?php echo $url . $lastpage ?>">Anterior</a>
+                            </li>
+                        <?php } ?>
+                        <!-- <li class="page-item active" aria-current="page"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">2</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li> -->
+                        <?php if ($currentpage != $finalpage) { ?>
+                            <li class="page-item">
+                                <a class="page-link" href="<?php echo $url . $nextpage ?>">Próxima</a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </nav>
+            <?php } ?>
             </div>
 
         </div>
