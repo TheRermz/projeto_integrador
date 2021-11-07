12b6b79a8d12d4da0bdc8159b870ad896ee25b42
TheRermz <mfischer74@gmail.com>
2021-11-07 03:09:14 -0300

    session logoff

---
+++ logoff.php
@@ -0,0 +1,8 @@
+<?php 
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+unset($_SESSION["user"], $_SESSION["type"]);
+$_SESSION["logoff"] = 'Logoff realizado com sucesso!';
+header('Location:signin.php');
